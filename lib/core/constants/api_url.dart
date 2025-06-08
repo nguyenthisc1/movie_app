56440128eea773096ca7575346c433845b961faa
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-09 00:58:06 +0700

    Add: .vscode run devices

--- lib/core/constants/api_url.dart
+++ lib/core/constants/api_url.dart
@@ -1,7 +1,8 @@
 class ApiUrl {
-  static const baseUrl =
-      // 'https://backend-netflix-production-c54a.up.railway.app/';
-      'http://localhost:8080/';
+  static const String baseUrl = String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: 'http://localhost:8080/',
+  );
 
   static const signup = 'api/v1/auth/signup';
   static const signin = 'api/v1/auth/signin';
