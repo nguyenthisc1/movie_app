be18e165d5694d9a80119819b43366db584d7174
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-11 21:16:25 +0700

    Add: movie api url

--- lib/core/constants/api_url.dart
+++ lib/core/constants/api_url.dart
@@ -24,6 +24,16 @@ class ApiUrl {
   // 	"command": "flutter run -d 'Chrome' --dart-define=BASE_URL=http://localhost:8080/"
   // }
 
+  // Auth
   static const signup = 'api/v1/auth/signup';
   static const signin = 'api/v1/auth/signin';
+
+  // Movies
+  static const moviesTrending = 'api/v1/movie/trending';
+  static const moviesNowPlaying = 'api/v1/movie/nowplaying';
+  static const movieTrailer = 'api/v1/movie/:id/trailer';
+  static const movieDetails = 'api/v1/movie/:id/details';
+  static const movieSimilar = 'api/v1/movie/:id/similar';
+  static const movieRecommendations = 'api/v1/movie/:id/recommendations';
+  static const moviesByCategory = 'api/v1/movie/:category';
 }
