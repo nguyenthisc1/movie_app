7c5b4d8ac6bcc35b2f54a6830ad8fec6210f330b
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-11 21:15:19 +0700

    Add: state authentication bloc

--- lib/presentation/splash/bloc/splash_cubic.dart
+++ lib/presentation/splash/bloc/splash_cubic.dart
@@ -1,11 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/domain/auth/usecaes/is_logged_in.dart';
 import 'package:movie_app/presentation/splash/bloc/splash_state.dart';
+import 'package:movie_app/service_locator.dart';
 
 class SplashCubic extends Cubit<SplashState> {
   SplashCubic() : super(DisplaySplash());
 
   void appStarted() async {
     await Future.delayed(const Duration(seconds: 2));
+    var isLoggedIn = await sl<IsLoggedInUseCase>().call();
+
+    if (isLoggedIn) {
+      emit(Authenticated());
+    } else {
+      emit(UnAuthenticated());
+    }
 
     emit(UnAuthenticated());
   }
