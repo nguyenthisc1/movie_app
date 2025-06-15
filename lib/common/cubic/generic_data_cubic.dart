d39dd020bc462aca30a0ed73c922d043c01de85b
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-15 17:55:51 +0700

    Update: genericData closed

--- lib/common/cubic/generic_data_cubic.dart
+++ lib/common/cubic/generic_data_cubic.dart
@@ -5,15 +5,29 @@ import 'package:movie_app/core/usecase/usecase.dart';
 class GenericDataCubit extends Cubit<GenericDataState> {
   GenericDataCubit() : super(DataLoading());
 
-  void getData<T>(UseCase usecase, {dynamic params}) async {
-    var returnedData = await usecase.call(params: params);
-    returnedData.fold(
-      (error) {
-        emit(FailureLoadData(errorMessage: error));
-      },
-      (data) {
-        emit(DataLoaded<T>(data: data));
-      },
-    );
+  Future<void> getData<T>(UseCase usecase, {dynamic params}) async {
+    if (isClosed) return;
+
+    try {
+      var returnedData = await usecase.call(params: params);
+      if (isClosed) return;
+
+      returnedData.fold(
+        (error) {
+          if (!isClosed) {
+            emit(FailureLoadData(errorMessage: error));
+          }
+        },
+        (data) {
+          if (!isClosed) {
+            emit(DataLoaded<T>(data: data));
+          }
+        },
+      );
+    } catch (e) {
+      if (!isClosed) {
+        emit(FailureLoadData(errorMessage: e.toString()));
+      }
+    }
   }
 }
