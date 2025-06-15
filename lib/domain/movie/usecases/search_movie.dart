6b7aeff0d5efc50ad59d19dea49b13fe7208f7ab
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-15 15:47:17 +0700

    Add: usecase movies api

---
+++ lib/domain/movie/usecases/search_movie.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/core/usecase/usecase.dart';
+import 'package:movie_app/domain/movie/repositories/movie_repository.dart';
+import 'package:movie_app/service_locator.dart';
+
+class SearchMovieUseCase extends UseCase<Either, dynamic> {
+  @override
+  Future<Either> call({params}) async {
+    return await sl<MovieRepository>().searchMovie(params!);
+  }
+}
