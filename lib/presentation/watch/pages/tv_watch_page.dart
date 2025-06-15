4185ad6d28607aea6d51c1c1ef32224c927d0554
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-15 17:59:21 +0700

    Update: tv watch page

--- lib/presentation/watch/pages/tv_watch_page.dart
+++ lib/presentation/watch/pages/tv_watch_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/common/wigets/appbar/appbar.dart';
 import 'package:movie_app/domain/tv/entities/tv_entity.dart';
+import 'package:movie_app/presentation/watch/widgets/keyword_tv.dart';
+import 'package:movie_app/presentation/watch/widgets/recomendation_tvs.dart';
+import 'package:movie_app/presentation/watch/widgets/similar_tvs.dart';
+import 'package:movie_app/presentation/watch/widgets/video_overview.dart';
 import 'package:movie_app/presentation/watch/widgets/video_player.dart';
 import 'package:movie_app/presentation/watch/widgets/video_title.dart';
 import 'package:movie_app/presentation/watch/widgets/video_vote_average.dart';
@@ -19,18 +23,18 @@ class TVWatchPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             VideoPlayer(id: tvEntity.id!),
-            const SizedBox(height: 16),
+            const SizedBox(height: 24),
             VideoTitle(title: tvEntity.name!),
-            const SizedBox(height: 16),
-            // TVKeywords(tvId: tvEntity.id!,),
-            const SizedBox(height: 16),
+            const SizedBox(height: 24),
+            KeywordTV(tvId: tvEntity.id!),
+            const SizedBox(height: 24),
             VideoVoteAverage(voteAverage: tvEntity.voteAverage!),
-            const SizedBox(height: 16),
-            // VideoOverview(overview: tvEntity.overview!,),
-            const SizedBox(height: 16),
-            // RecommendationTVs(tvId: tvEntity.id!,),
-            const SizedBox(height: 16),
-            // SimilarTVs(tvId: tvEntity.id!,)
+            const SizedBox(height: 36),
+            VideoOverview(overview: tvEntity.overview!),
+            const SizedBox(height: 36),
+            RecommendationTVs(tvId: tvEntity.id!),
+            const SizedBox(height: 36),
+            SimilarTVs(tvId: tvEntity.id!),
           ],
         ),
       ),
